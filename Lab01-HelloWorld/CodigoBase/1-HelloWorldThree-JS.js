e59992d03123bbe67299fa0738de8e808bd7db0f
Aluno de EA979 <ea979@le27-15.grad.fee.unicamp.br>
2023-06-01 14:13:06 -0300

    Update of files

--- Lab01-HelloWorld/CodigoBase/1-HelloWorldThree-JS.js
+++ Lab01-HelloWorld/CodigoBase/1-HelloWorldThree-JS.js
@@ -6,14 +6,14 @@ function main() {
 
 	var renderer = new THREE.WebGLRenderer();
 
-	renderer.setClearColor(new THREE.Color(0.0, 0.0, 0.0));
-	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.setClearColor(new THREE.Color(1.0, 0.8, 1.0));
+	renderer.setSize(window.innerWidth/2, window.innerHeight);
 
 	document.getElementById("threejs-canvas").appendChild(renderer.domElement);
 	document.getElementById("output-text").innerHTML += " (<i>release " + THREE.REVISION + "</i>)";	
 
-	var scene 	= new THREE.Scene();
 	var camera 	= new THREE.Camera();
+	var scene 	= new THREE.Scene();
 
 	renderer.render(scene, camera);
 };
--- Lab01-HelloWorld/CodigoBase/6-Geometria3D.js
+++ Lab01-HelloWorld/CodigoBase/6-Geometria3D.js
@@ -6,12 +6,15 @@ import { GUI } from '../../Assets/scripts/three.js/examples/jsm/libs/lil-gui.mod
 
 const 	gui 		= new GUI();
 const 	rendSize 	= new THREE.Vector2();
+const clock = new THREE.Clock();
 
 var 	controls, 
 		scene,
 		camera,
 		renderer,
-		curObj = null;
+		animeID,
+		objMesh,
+		curObj;
 
 // ******************************************************************** //
 // **                                                                ** //
@@ -19,30 +22,21 @@ var 	controls,
 function main() {
 
 	renderer = new THREE.WebGLRenderer();
-
 	renderer.setClearColor(new THREE.Color(0.0, 0.0, 0.0));
-
 	rendSize.x = 
 	rendSize.y = Math.min(window.innerWidth, window.innerHeight) * 0.8;
-
 	renderer.setSize(rendSize.x, rendSize.y);
-
 	document.body.appendChild(renderer.domElement);
-
 	window.addEventListener ( 'resize', onWindowResize 	);
-
 	scene 	= new THREE.Scene();
-
 	initGUI();
-
 	camera = new THREE.OrthographicCamera( -1.0, 1.0, 1.0, -1.0, -1.0, 1.0 );
 
 	var axis = new THREE.AxesHelper(0.8);
     axis.name = "eixos";
     scene.add(axis);
 
-
-	var objMesh = new THREE.Mesh 	( 	new THREE.TetrahedronGeometry(), 
+	objMesh = new THREE.Mesh 	( 	new THREE.TetrahedronGeometry(), 
 										new THREE.MeshBasicMaterial({color:0xff0000, wireframe:true })
 									); 
 	objMesh.name 	= "tetraedro";
@@ -50,7 +44,6 @@ function main() {
 	objMesh.rotateY(60.0 * Math.PI / 180.0); 
 	objMesh.updateMatrix();
 	scene.add( objMesh );
-
 	curObj = objMesh;
 	
 	objMesh = new THREE.Mesh 	( 	new THREE.TorusGeometry(0.5, 0.3, 30, 30), 
@@ -60,14 +53,14 @@ function main() {
 	objMesh.visible = false;
 	scene.add( objMesh );
 	
-	var objMesh = new THREE.Mesh 	( 	new THREE.TorusKnotGeometry(0.5, 0.2), 
+	objMesh = new THREE.Mesh 	( 	new THREE.TorusKnotGeometry(0.5, 0.2), 
 										new THREE.MeshBasicMaterial({color:0x0000ff, wireframe:true })
 									); 
 	objMesh.name 	= "TorusKnot";
 	objMesh.visible = false;
-	// objMesh.rotateX(60.0 * Math.PI / 180.0); 
-	// objMesh.rotateY(30.0 * Math.PI / 180.0); 
-	// objMesh.updateMatrix();
+	objMesh.rotateX(270.0 * Math.PI / 180.0); 
+	objMesh.rotateY(30.0 * Math.PI / 180.0); 
+	objMesh.updateMatrix();
 	scene.add( objMesh );
 
 	renderer.clear();
@@ -79,12 +72,15 @@ function main() {
 // ******************************************************************** //
 function initGUI() {
 
-	var controls = 	{	Forma3D : "Tetraedro"
+	var controls = 	{	Forma3D : "Tetraedro",
+						animation: false
 					};
 
-	gui.add( controls, 'Forma3D', [ 	"Tetraedro", 
+	gui.add( controls, 'Forma3D', [ 	
+		"Tetraedro", 
 										"Toro", 
 										"TorusKnot" ] ).onChange(changeObj);
+	gui.add( controls, 'animation').onChange(animation);
 	gui.open();
 };
 
@@ -106,6 +102,7 @@ function changeObj(val) {
 								scene.getObjectByName("TorusKnot").visible 	= false;
 								break;
 		case "TorusKnot"	:  	curObj = scene.getObjectByName("TorusKnot");
+		// curObj.rotateX(50 * Math.PI / 180.0)
 								curObj.visible							 	= true;
 								scene.getObjectByName("tetraedro").visible 	= false;
 								scene.getObjectByName("toro").visible 		= false;
@@ -119,6 +116,31 @@ function changeObj(val) {
 // ******************************************************************** //
 // **                                                                ** //
 // ******************************************************************** //
+function animation(value) {
+
+	if (value) 
+		animeID = requestAnimationFrame(anime);
+	else {
+		cancelAnimationFrame(animeID);
+		renderer.clear();
+		renderer.render(scene, camera);	
+		oldTime = 0;
+		}
+}
+
+function anime() {
+
+	const delta = clock.getDelta();
+	curObj.rotateX(10*delta * Math.PI / 180.0); 
+	curObj.rotateY(10*delta * Math.PI / 180.0); 
+	curObj.rotateZ(10*delta * Math.PI / 180.0); 
+	curObj.updateMatrix();
+	renderer.clear();
+	renderer.render(scene, camera);	
+	animeID = requestAnimationFrame(anime);
+}
+
+
 function onWindowResize() {
 
 	let minDim = Math.min(window.innerWidth, window.innerHeight);
